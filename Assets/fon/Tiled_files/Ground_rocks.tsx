d49344c3d948e1505bed50af5c6c66dcb21d1c40
ZSPN1111 <tmmratomtm@gmail.com>
2024-10-07 21:29:25 +0300

    123

--- Assets/fon/Tiled_files/Ground_rocks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Ground_rocks" tilewidth="16" tileheight="16" tilecount="1147" columns="31">
- <image source="../PNG/Ground_rocks.png" width="496" height="592"/>
-</tileset>
