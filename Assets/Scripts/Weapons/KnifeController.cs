9424318a7564ed2f9016984ab95e6fed475e5ea3
Qermon <106011673+Qermon@users.noreply.github.com>
2024-10-08 14:30:21 +0300

    папка

---
+++ Assets/Scripts/Weapons/KnifeController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeController : WeaponController
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        GameObject spawnedKnife = Instantiate(prefab);
+        spawnedKnife.transform.position = transform.position; //Assign the position to be the same as this object which is parented to the player
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base script for all weapon controllers
+/// </summary>
+public class WeaponController : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public GameObject prefab;
+    public float damage;
+    public float speed;
+    public float cooldownDuration;
+    float currentCooldown;
+    public int pierce;
+
+
+    protected virtual void Start()
+    {
+        currentCooldown = cooldownDuration; //At the start set the current cooldown to be cooldown duration
+    }
+
+    protected virtual void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+        if (currentCooldown <= 0f)   //Once the cooldown becomes 0, attack
+        {
+            Attack();
+        }
+    }
+
+    protected virtual void Attack()
+    {
+        currentCooldown = cooldownDuration;
+    }
+}
